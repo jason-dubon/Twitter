1b3ca07a82e6059b043278121573ce6f2c8e7ad3
Jason Dubon <jasonsdubon@gmail.com>
2022-07-17 07:32:11 -0400

    tweet view

--- Twitter/Core/Components/Tweets/TweetRowView.swift
+++ Twitter/Core/Components/Tweets/TweetRowView.swift
@@ -9,7 +9,69 @@ import SwiftUI
 
 struct TweetRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading) {
+            // profile image, and tweet
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color(.systemRed))
+                
+                // Tweet Info
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Rocky")
+                            .font(.subheadline).bold()
+                        Text("@thedog")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        Text("19s")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    
+                    // tweet caption/content
+                    Text("I want some food.")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            
+            // action buttons
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "message")
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "heart")
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bookmark")
+                }
+                
+            }.padding(7)
+            Divider()
+        }.padding()
     }
 }
 
